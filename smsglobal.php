6192f69a3da202bab54ff5b2c68abad056876a60
Kane <kane@smsglobal.com>
2013-08-16 09:23:20 +1000

    Added custom field to user profile for mobile number

--- smsglobal.php
+++ smsglobal.php
@@ -18,6 +18,16 @@ register_activation_hook( __FILE__, 'smsglobal_install' );
 register_activation_hook( __FILE__, 'smsglobal_install_data' );
 
 load_plugin_textdomain('smsglobal', false, basename($dir) . '/languages');
+
+function modify_contact_methods($profile_fields) {
+
+    // Add new fields
+    $profile_fields['mobile'] = 'Mobile Phone Number';
+
+    return $profile_fields;
+}
+add_filter('user_contactmethods', 'modify_contact_methods');
+
 new Smsglobal_SettingsPage();
 new Smsglobal_SmsPage();
 
